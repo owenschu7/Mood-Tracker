d15ee11e10877b95d5d124081dc2a10b30d775de
Owen Schumacher <owenschu7@gmail.com>
2025-03-02 23:37:28 -0800

    added questions

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 
 int main() {
 
-	std::cout >> "hello world\n";
-
-
+	std::cout >> "Welcome to Mood Tracker\n";
+	std::cout >> "How are you feeling today? 1-10: ";
+	std::cout >> "Energy level? 1-10: ";
+        std::cout >> "Physical level? 1-10: ";
+        std::cout >> "Did you take your vitamins/meds yesterday? yes(0)/no(1) ";
+        std::cout >> "How did you eat yesterday? 1-10: ";
+        std::cout >> "How long did you sleep the night before? ex: 6.5 : ";
 
 
 }
